dad41df5127ebd1864a5d29d31246099e4b06718
dbfjru <dbfjru@gmail.com>
2024-01-23 02:50:04 +0900

    init & MySQL connection

---
+++ src/main/java/com/sparta/myscheduleapp/MyScheduleAppApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myscheduleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyScheduleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyScheduleAppApplication.class, args);
+    }
+
+}
